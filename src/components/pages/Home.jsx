66f2fe3948798fc7cc88fc46285211016aabf4ed
Carl Nelson <99733202+CNelson0306@users.noreply.github.com>
2025-02-13 14:13:24 +0000

    First commit

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -8,12 +8,30 @@ function Home () {
     <div className='display'>
         <h1>Hair & Beauty</h1>
         <h2>Established 2015</h2>
-          <div className='bio'>
-            <p>Bio</p>
+          <div className='intro'>
+            <p>Fully Qualified & Insured</p>
+            <p>Award Winning Salon</p>
+            <br />
+            <br />
+            <div className='bio'>
+              <p>Since founding Fuschia Hive Hair & Beauty in 2015 I have been dedicated to</p>
+              <p>delivering exceptional hair and beauty services that leave every client feeling confident</p>
+              <p>and pampered. With a passion for the industry and an eye for detail, I have earned</p>
+              <p>multiple awards, showcasing expertise and a commitment to excellence.</p>
+              <br />
+              <p>Backed by extensive qualifications across a variety of hair and beauty services, I offer</p>
+              <p>everything from blow drys, nails, waxing, tinting to complete transformations.</p>
+              <p>Whether you're looking for a simple refresh or a dramatic makeover, I will be with</p>
+              <p>you every step of the way making you feel your best.</p>
+              <br />
+              <p>At Fuschia Hive, it's not just about skills, it's about making every client feel heard,</p>
+              <p>valued and truly satisfied.  Your satisfaction is always my top priority.</p>
+              <br />
+              <p>I aim to create the perfect experience whether it would be for a special occasion,</p>
+              <p>or you just need a little pampering, the experience is tailored for you.</p>
+              </div>
           </div>
             <div className='intro'>
-              <p>Fully Qualified & Insured</p>
-              <p>Award Winning Salon</p>
               <p>Voted Best In swansea & people's choice in 2022</p>
             </div>
             <div className='awards'>
