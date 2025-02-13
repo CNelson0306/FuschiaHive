d8615741cd892eb07ab8dfb0f1f69f9b7374e455
Carl Nelson <99733202+CNelson0306@users.noreply.github.com>
2025-02-13 09:12:33 +0000

    First Commit

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Home.css';
-import swansea from '../../assets/bestSwansea.png';
-import people from '../../assets/peoplesChoice.png';
+import swansea from '../../assets/bestSwansea.webp'
+import people from '../../assets/peoplesChoice.webp';
 
 function Home () {
   return (
