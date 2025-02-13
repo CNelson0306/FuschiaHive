f81b37270c2170a35e801536193e7a4f29e73fbe
Carl Nelson <99733202+CNelson0306@users.noreply.github.com>
2025-02-13 13:46:21 +0000

    First commit

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -8,20 +8,22 @@ function Home () {
     <div className='display'>
         <h1>Hair & Beauty</h1>
         <h2>Established 2015</h2>
-        
-        <div className='intro'>
-          <p>Fully Qualified & Insured</p>
-          <p>Award Winning Salon</p>
-          <p>Voted Best In swansea & people's choice in 2022</p>
-        </div>
-        <div className='awards'>
-            <div className='awardSwansea'>
-                <img src={swansea} />
+          <div className='bio'>
+            <p>Bio</p>
+          </div>
+            <div className='intro'>
+              <p>Fully Qualified & Insured</p>
+              <p>Award Winning Salon</p>
+              <p>Voted Best In swansea & people's choice in 2022</p>
             </div>
-            <div className='awardPeople'>
-                <img src={people} />
-            </div>
-        </div>
+            <div className='awards'>
+                <div className='awardSwansea'>
+                    <img src={swansea} />
+                </div>
+                <div className='awardPeople'>
+                    <img src={people} />
+                </div>
+          </div>
     </div>
   )
 }
